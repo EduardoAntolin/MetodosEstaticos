bd0dd04a0f45924587255f6242f2779687212e43
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2018-08-17 07:41:46 -0700

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Matematicas.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Matematicas
+    {
+        //2^3 = eit
+        public static int Potencia(int numeroBase, int exponente)
+        {
+            int resultado = 1; //numeroBase;
+           for(int i=0; i < exponente; i++)
+            {
+                resultado = resultado * numeroBase;
+            }
+            return resultado;
+        }
+    }
+}
